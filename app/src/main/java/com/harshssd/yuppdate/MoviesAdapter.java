fee898a794674e443f75528869928c013be6ee76
Harsha <harsh.ssd@gmail.com>
2016-12-08 23:49:25 -0800

    Create a MoviesAdapter class outside of the Activity.

---
+++ app/src/main/java/com/harshssd/yuppdate/MoviesAdapter.java
@@ -0,0 +1,85 @@
+package com.harshssd.yuppdate;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.harshssd.yuppdate.dummy.DummyContent;
+
+import java.util.List;
+
+/**
+ * Created by Harsha on 12/8/16.
+ */
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
+
+    private final List<DummyContent.DummyItem> mValues;
+
+    public MoviesAdapter(List<DummyContent.DummyItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_list_content, parent, false);
+        return new MoviesAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getContext().getResources().getBoolean(R.bool.twoPaneMode)) {
+                    Bundle arguments = new Bundle();
+                    arguments.putString(MovieDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+                    MovieDetailFragment fragment = new MovieDetailFragment();
+                    fragment.setArguments(arguments);
+                    ((FragmentActivity) v.getContext()).getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.movie_detail_container, fragment)
+                            .commit();
+                } else {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, MovieDetailActivity.class);
+                    intent.putExtra(MovieDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+
+                    context.startActivity(intent);
+                }
+            }
+        });    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public DummyContent.DummyItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.id);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
