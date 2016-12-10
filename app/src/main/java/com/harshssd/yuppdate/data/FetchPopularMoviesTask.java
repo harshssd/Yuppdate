20881f5e7b042ef5704247d31cff43426e752391
Harsha <harsh.ssd@gmail.com>
2016-12-10 12:44:39 -0800

    Create an Asynchronous Task to call the Popular Movies API.

---
+++ app/src/main/java/com/harshssd/yuppdate/data/FetchPopularMoviesTask.java
@@ -0,0 +1,79 @@
+package com.harshssd.yuppdate.data;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Asynchronous Task to fetch the Popular Movies using MovieDB API.
+ * https://www.themoviedb.org/documentation/api
+ *
+ * @author Harsha
+ */
+public class FetchPopularMoviesTask extends AsyncTask {
+
+    private final String LOG_TAG = FetchPopularMoviesTask.class.getSimpleName();
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        try {
+            final String MOVIE_DB_BASE_URL = "https://api.themoviedb.org/3/movie/popular?";
+            final String API_KEY = "api_key";
+            Uri movieDbUri = Uri.parse(MOVIE_DB_BASE_URL).buildUpon()
+                    .appendQueryParameter(API_KEY, "<API_KEY>")
+                    .build();
+            URL movieDbUrl = new URL(movieDbUri.toString());
+
+            // Create the request to MovieDB API, and open the connection
+            urlConnection = (HttpURLConnection) movieDbUrl.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+                Log.v(LOG_TAG, line + "\n");
+
+            }
+            if (buffer.length() == 0) {
+                return null;
+            }
+            return buffer.toString();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error ", e);
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
+        }
+    }
+}
