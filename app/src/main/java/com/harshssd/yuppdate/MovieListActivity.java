39cea3833b897c195d11a78d3fe4a6ea5ba5293b
Harsha <harsh.ssd@gmail.com>
2016-12-18 01:17:50 -0800

    Add Sort Order Key in the Settings Menu option.

--- app/src/main/java/com/harshssd/yuppdate/MovieListActivity.java
+++ app/src/main/java/com/harshssd/yuppdate/MovieListActivity.java
@@ -1,5 +1,6 @@
 package com.harshssd.yuppdate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.harshssd.yuppdate.data.FetchPopularMoviesTask;
@@ -65,5 +68,25 @@ public class MovieListActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         recyclerView.setAdapter(new MoviesAdapter(mMovies));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.movie_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
